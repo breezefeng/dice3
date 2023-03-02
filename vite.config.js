50e9e03918e7d666b1eca337b6ad087098593c35
fengyuqing2012 <fengyuqing2012@dingtalk.com>
2023-03-02 17:52:55 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production'
+    ? '//mobile.helensbar.com/test/diceGame/' // 测试环境
+    // ? '//mobile.helensbar.com/integrityHandover/'   // 正式环境
+    : '/'
 })
